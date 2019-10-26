73ae7d775bb4de3c78e34b3e1a2b1722cf8b83fb
Michael Argent <michael.r.argent@gmail.com>
2019-10-26 14:48:06 -0500

    fixed issue with added items

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ $(function(){
             </div>
           </li>`
           $('.shopping-list').append(newItem);
+          $('#shopping-list-entry').val('');
         });
  
-        $('.shopping-item-delete').on('click', function(event) {
+        $('ul').on('click', '.shopping-item-delete', function(event) {
             $(event.target).closest('li').remove(); //target is the item that is selected
         });
 
-        $('.shopping-item-toggle').on('click', function(event) {
+        $('ul').on('click', '.shopping-item-toggle', function(event) {
             //when check is clicked
             //item becomes crossed out
             $(event.target).closest('li').children('span').toggleClass('shopping-item__checked');
